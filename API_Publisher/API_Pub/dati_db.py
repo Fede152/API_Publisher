9600a3478ecac7aa1e5c45031566e16e2459ed30
Federico <federico.brugnano@fermi.mo.it>
2025-11-22 08:45:32 +0100

    Modifica nei dati del database, inserita la variabile pub_id nei dati.

--- API_Publisher/API_Pub/dati_db.py
+++ API_Publisher/API_Pub/dati_db.py
@@ -38,105 +38,105 @@ async def doc():
     "author": "Italo Calvino",
     "genre": "Romanzo",
     "year": 1957,
-    "publisher_id": "OBJECT_ID_EINAUDI"
-  },
+    "publisher_id": pub_id
+    },
   {
     "title": "Se una notte d'inverno un viaggiatore",
     "author": "Italo Calvino",
     "genre": "Romanzo",
     "year": 1979,
-    "publisher_id": "OBJECT_ID_EINAUDI"
+    "publisher_id": pub_id
   },
   {
     "title": "Il nome della rosa",
     "author": "Umberto Eco",
     "genre": "Giallo",
     "year": 1980,
-    "publisher_id": "OBJECT_ID_EINAUDI"
+    "publisher_id": pub_id
   },
   {
     "title": "Il codice da Vinci",
     "author": "Dan Brown",
     "genre": "Giallo",
     "year": 2003,
-    "publisher_id": "OBJECT_ID_PENGUIN"
+    "publisher_id": pub_id
   },
   {
     "title": "Harry Potter e la pietra filosofale",
     "author": "J.K. Rowling",
     "genre": "Fantasy",
     "year": 1997,
-    "publisher_id": "OBJECT_ID_PENGUIN"
+    "publisher_id": pub_id
   },
   {
     "title": "Il signore degli anelli",
     "author": "J.R.R. Tolkien",
     "genre": "Fantasy",
     "year": 1954,
-    "publisher_id": "OBJECT_ID_PENGUIN"
+    "publisher_id": pub_id
   },
   {
     "title": "1984",
     "author": "George Orwell",
     "genre": "Romanzo",
     "year": 1949,
-    "publisher_id": "OBJECT_ID_MONDADORI"
+    "publisher_id": pub_id
   },
   {
     "title": "Hunger Games",
     "author": "Suzanne Collins",
     "genre": "Fantasy",
     "year": 2008,
-    "publisher_id": "OBJECT_ID_MONDADORI"
+    "publisher_id": pub_id
   },
   {
     "title": "La ragazza del treno",
     "author": "Paula Hawkins",
     "genre": "Giallo",
     "year": 2015,
-    "publisher_id": "OBJECT_ID_MONDADORI"
+    "publisher_id": pub_id
   },
   {
     "title": "Harry Potter e il prigioniero di Azkaban",
     "author": "J.K. Rowling",
     "genre": "Fantasy",
     "year": 1999,
-    "publisher_id": "OBJECT_ID_HARPERCOLLINS"
+    "publisher_id": pub_id
   },
   {
     "title": "Il piccolo principe",
     "author": "Antoine de Saint-Exupéry",
     "genre": "Romanzo",
     "year": 1943,
-    "publisher_id": "OBJECT_ID_HARPERCOLLINS"
+    "publisher_id": pub_id
   },
   {
     "title": "Il vecchio e il mare",
     "author": "Ernest Hemingway",
     "genre": "Romanzo",
     "year": 1952,
-    "publisher_id": "OBJECT_ID_HARPERCOLLINS"
+    "publisher_id": pub_id
   },
   {
     "title": "Sostiene Pereira",
     "author": "Antonio Tabucchi",
     "genre": "Romanzo",
     "year": 1994,
-    "publisher_id": "OBJECT_ID_FELTRINELLI"
+    "publisher_id": pub_id
   },
   {
     "title": "La ragazza del treno",
     "author": "Paula Hawkins",
     "genre": "Giallo",
     "year": 2015,
-    "publisher_id": "OBJECT_ID_FELTRINELLI"
+    "publisher_id": pub_id
   },
   {
     "title": "Cecità",
     "author": "José Saramago",
     "genre": "Romanzo",
     "year": 1995,
-    "publisher_id": "OBJECT_ID_FELTRINELLI"
+    "publisher_id": pub_id
   }])
 
 asyncio.run(doc())
