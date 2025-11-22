a04b7f18b8574df9d1fdd924789f14d2e5c45916
Federico <federico.brugnano@fermi.mo.it>
2025-11-22 08:24:59 +0100

    creazione struttura Handler per le case editrici con implementazione di get, post, put e delete.

--- API_Publisher/API_Pub/main1.py
+++ API_Publisher/API_Pub/main1.py
@@ -15,3 +15,39 @@ books_collection = db["books"]
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Benvenuto nella API Publisher-Books, da qui puoi avviare la tua ricerca che sia tramite le case editrici o tramite i libri!")
+
+
+# Handler per le case editrici
+class PublisherHandler(tornado.web.RequestHandler):
+    def get(self, publisher_id=None):
+        query = {}
+        if publisher_id:
+            query['_id'] = ObjectId(publisher_id)
+        else:
+            name = self.get_argument('name', None)
+            country = self.get_argument('country', None)
+            if name:
+                query['name'] = name
+            if country:
+                query['country'] = country
+        publishers = list(publishers_collection.find(query))
+        for p in publishers:
+            p['_id'] = str(p['_id'])
+        self.set_header("Content-Type", "application/json")
+        self.write(json.dumps(publishers))
+
+    def post(self):
+        data = json.loads(self.request.body)
+        result = publishers_collection.insert_one(data)
+        self.set_header("Content-Type", "application/json")
+        self.write({"inserted_id": str(result.inserted_id)})
+
+    def put(self, publisher_id):
+        data = json.loads(self.request.body)
+        publishers_collection.update_one({'_id': ObjectId(publisher_id)}, {'$set': data})
+        self.write({"status": "updated"})
+
+    def delete(self, publisher_id):
+        publishers_collection.delete_one({'_id': ObjectId(publisher_id)})
+        self.write({"status": "deleted"})
+
